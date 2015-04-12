e1933ae421ca029279da4047565f87c4b8a1e1eb
adamrenklint <adam@renklint.com>
2015-04-12 11:17:01 +0200

    use ampersand-state, start with Note

--- examples/metronome/main.js
+++ examples/metronome/main.js
@@ -6,19 +6,26 @@ var bap = require('../../index');
 //   'release': 0.1,
 //   'length': 0.1
 // });
+// // simple way
 // var pling = kit.slot(1).layer(basic.with({ 'frequency': 330 }));
-// var plong = kit.slot(2).layer(basic.with({ 'frequency': 440 }));
+// // more verbose: create, build, then assign
+// var nextSlot = bap.slot();
+// nextSlot.layer(basic.with({ 'frequency': 440 }));
+// var plong = kit.slot(2, nextSlot);
 
 var pattern = bap.pattern(/*1 bar, 4 beats per bar*/);
 pattern.channel(1).add(
   ['*.1.01', 'A1'],
+  ['*.2.01', 'A2'],
+  ['*.3.01', 'A2'],
+  ['*.4.01', 'A2']
   // ['*.2+.01', 'A2', 10]
-  ['*.2%1', 'A2']
+  // ['*.!1.01', 'A2']
 );
 
 // pattern are automatically looped, sequences are not
 // pattern.use('A', kit).start();
-// pattern.start();
+pattern.start();
 
 var positionEl = document.getElementById('position');
 function draw () {
--- test/lib/Bap.test.js
+++ test/lib/Bap.test.js
@@ -1,16 +1,22 @@
 // var chai = require('chai');
 // var expect = chai.expect;
 // var Bap = require('../../lib/Bap');
+// var AudioContextMock = require('../../node_modules/dilla/test/mocks/AudioContext');
+// var audioContextMock = new AudioContextMock();
 //
 // var bap;
 //
 // before(function () {
-//   bap = new Bap();
+//   bap = new Bap({
+//     'context': audioContextMock
+//   });
 // });
 //
 // describe('lib/Bap', function () {
 //
-//   it('should expose audioContext', function () {
-//     expect(bap.audioContext).to.be.a('object');
+//   ['kit', 'pattern'].forEach(function (name) {
+//     it('should expose factory for "' + name + '"', function () {
+//       expect(bap[name]).to.be.a('function');
+//     });
 //   });
 // });
