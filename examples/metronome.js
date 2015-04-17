00a1b10e20ec44900cfb410233f85e088a902653
adamrenklint <adam@renklint.com>
2015-04-17 22:19:36 +0200

    use note param on oscillator to set frequency

--- examples/metronome.js
+++ examples/metronome.js
@@ -9,14 +9,14 @@ function metronome () {
   });
 
   // simple way
-  var pling = kit.slot(1).layer(basic.with({ 'frequency': 330 }));
+  var pling = kit.slot(1).layer(basic.with({ frequency: 330 }));
 
   // more verbose: create, build, then assign
   var nextSlot = bap.slot();
-  nextSlot.layer(basic.with({ 'frequency': 440 }));
+  nextSlot.layer(basic.with({ frequency: 440 }));
   kit.slot(2, nextSlot);
 
-  var pattern = bap.pattern({ 'bars': 2, 'tempo': 140 });
+  var pattern = bap.pattern({ bars: 2, tempo: 140 });
   pattern.channel(1).add(
     ['*.1.01', 'A1'],
     ['*.2%1.01', 'A2']
