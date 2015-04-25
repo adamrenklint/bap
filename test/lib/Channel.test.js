d3b716e45f8bcd4133a18f9ccf42f07bdda6c5aa
adamrenklint <adam@renklint.com>
2015-04-25 21:53:40 +0200

    use node-based unit tests until I actually need the browser...

---
+++ test/lib/Channel.test.js
@@ -0,0 +1,13 @@
+var chai = require('chai');
+var expect = chai.expect;
+var Channel = require('../../lib/Channel');
+
+var channel;
+
+beforeEach(function () {
+  channel = new Channel();
+});
+
+describe('Channel', function () {
+
+});
---
+++ test/lib/Sequence.test.js
@@ -0,0 +1,13 @@
+var chai = require('chai');
+var expect = chai.expect;
+var Sequence = require('../../lib/Sequence');
+
+var sequence;
+
+beforeEach(function () {
+  sequence = new Sequence();
+});
+
+describe('Sequence', function () {
+
+});
