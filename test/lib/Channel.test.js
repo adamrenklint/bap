bbf5ebccb6b5e8512916ecff3d5afd0545704746
adamrenklint <adam@renklint.com>
2015-04-26 16:29:39 +0200

    100% coverage for bap.channel

--- test/lib/Channel.test.js
+++ test/lib/Channel.test.js
@@ -1,7 +1,11 @@
 var chai = require('chai');
 var expect = chai.expect;
-var Channel = require('../../lib/Channel');
+var sinon = require('sinon');
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
 
+var Channel = require('../../lib/Channel');
+var Note = require('../../lib/Note');
 var channel;
 
 beforeEach(function () {
@@ -10,4 +14,48 @@ beforeEach(function () {
 
 describe('Channel', function () {
 
+  describe('_start(time, note)', function () {
+    it('should trigger "start" event', function () {
+      var spy = sinon.spy();
+      channel.on('start', spy);
+      channel._start(1, {});
+      expect(spy).to.have.been.calledOnce;
+    });
+    describe('when channel is muted', function () {
+      it('should not trigger "start" event', function () {
+        var spy = sinon.spy();
+        channel.on('start', spy);
+        channel.mute = true;
+        channel._start(1, {});
+        expect(spy).not.to.have.been.called;
+      });
+    });
+  });
+
+  describe('_stop(time, note)', function () {
+    it('should trigger "stop" event', function () {
+      var spy = sinon.spy();
+      channel.on('stop', spy);
+      channel._stop(1, {});
+      expect(spy).to.have.been.calledOnce;
+    });
+  });
+
+  describe('add(note, ...)', function () {
+    describe('when note is a raw object', function () {
+      it('should create a note instance', function () {
+        channel.add(['1.1.01', 'A1']);
+        expect(channel.notes.length).to.equal(1);
+        expect(channel.notes.models[0].type).to.equal('note');
+      });
+    });
+    describe('when passing a note instance', function () {
+      it('should just add the note', function () {
+        var note = new Note({ position: '2.1.02', key: 'B5' });
+        channel.add(note);
+        expect(channel.notes.length).to.equal(1);
+        expect(channel.notes.models[0]).to.equal(note);
+      });
+    });
+  });
 });
