e48d1636b97825a8efd4c26c903b8ea9ad9b508e
adamrenklint <adam@renklint.com>
2015-04-26 13:19:42 +0200

    100% coverage for bap.sequence

--- test/lib/Sequence.test.js
+++ test/lib/Sequence.test.js
@@ -2,6 +2,9 @@ var chai = require('chai');
 var expect = chai.expect;
 var Sequence = require('../../lib/Sequence');
 var Pattern = require('../../lib/Pattern');
+var sinon = require('sinon');
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
 
 var sequence;
 
@@ -29,6 +32,43 @@ describe('Sequence', function () {
           expect(seq.loop).to.be.true;
         });
       })
+    });
+  });
+
+  describe('sequences', function () {
+    describe('when setting sequences', function () {
+      it('should listen and forward changes on the new children', function () {
+        var pattern = new Pattern();
+        var seq = new Sequence();
+        var spy = sinon.spy();
+        seq.on('change:sequences', spy);
+        seq.sequences = [[pattern]];
+        expect(spy).to.have.been.calledOnce;
+        pattern.trigger('change:channels');
+        expect(spy).to.have.been.calledTwice;
+      });
+      it('should stop listening for changes on the previous children', function () {
+        var pattern = new Pattern();
+        var seq = new Sequence();
+        var spy = sinon.spy();
+        seq.on('change:sequences', spy);
+        seq.sequences = [[pattern]];
+        expect(spy).to.have.been.calledOnce;
+        var pattern2 = new Pattern();
+        seq.sequences = [[pattern2]];
+        expect(spy).to.have.been.calledTwice;
+        pattern.trigger('change:channels');
+        expect(spy).to.have.been.calledTwice;
+      });
+      describe('when trying to set something other than an array of arrays of sequences', function () {
+        it('should throw a meaningful error', function () {
+          expect(function () {
+            var seq = new Sequence();
+            var pattern = new Pattern();
+            seq.sequences = [pattern];
+          }).to.throw(/Property 'sequences' must be of type sequence-array/);
+        });
+      });
     });
   });
 
@@ -81,9 +121,6 @@ describe('Sequence', function () {
         pattern1.channel(1).add(['1.1.01', 'A1']);
         pattern2.channel(1).add(['*.1.05', 'B1']);
         var seq1 = new Sequence(pattern1, [pattern1, pattern2]);
-        // console.log(seq1.notes()[2].position);
-        // return;
-        global.doStep = true;
         var seq2 = new Sequence(seq1, seq1);
         var notes = seq2.notes();
         expect(notes.length).to.equal(6);
@@ -99,8 +136,69 @@ describe('Sequence', function () {
     });
   });
 
+  describe('tempo', function () {
+    it('should return the tempo of the first sequence', function () {
+      var pattern = new Pattern({ tempo: 95 });
+      var pattern2 = new Pattern({ tempo: 105 });
+      var pattern3 = new Pattern({ tempo: 25 });
+      var seq = new Sequence([pattern, pattern2], pattern3);
+      expect(seq.tempo).to.equal(95);
+    });
+    describe('when there is no sequences', function () {
+      it('should return the global default (120)', function () {
+        var seq = new Sequence();
+        expect(seq.tempo).to.equal(120);
+      });
+    });
+  });
+
+  describe('bars', function () {
+    it('should return the length of all child sequences', function () {
+      var pattern = new Pattern({ tempo: 95 });
+      var pattern2 = new Pattern({ bars: 4, tempo: 105 });
+      var pattern3 = new Pattern({ tempo: 25 });
+      var seq = new Sequence([pattern, pattern2], pattern3);
+      expect(seq.bars).to.equal(5);
+    });
+    describe('when there are no child sequences', function () {
+      it('should return 0', function () {
+        var seq = new Sequence();
+        expect(seq.bars).to.equal(0);
+      });
+    });
+  });
+
+  describe('beatsPerBar', function () {
+    it('should return the beatsPerBar of the first sequence', function () {
+      var pattern = new Pattern({ beatsPerBar: 5 });
+      var pattern2 = new Pattern({ beatsPerBar: 6 });
+      var pattern3 = new Pattern({ beatsPerBar: 2 });
+      var seq = new Sequence([pattern, pattern2], pattern3);
+      expect(seq.beatsPerBar).to.equal(5);
+    });
+    describe('when there is no sequences', function () {
+      it('should return the global default (4)', function () {
+        var seq = new Sequence();
+        expect(seq.beatsPerBar).to.equal(4);
+      });
+    });
+  });
+
   describe('tempoChanges()', function () {
-    it('should return ')
+    it('should return all tempo changes', function () {
+      var pattern = new Pattern({ tempo: 95 });
+      var pattern2 = new Pattern({ tempo: 105 });
+      var pattern3 = new Pattern({ tempo: 25 });
+      var seq = new Sequence([pattern, pattern2], pattern3, pattern3);
+      var changes = seq.tempoChanges();
+      expect(changes.length).to.equal(3);
+      expect(changes[0][0]).to.equal('1.1.01');
+      expect(changes[0][1].val).to.equal(95);
+      expect(changes[1][0]).to.equal('1.4.96');
+      expect(changes[1][1].val).to.equal(25);
+      expect(changes[2][0]).to.equal('3.4.96');
+      expect(changes[2][1].val).to.equal(95);
+    });
   });
 
   describe('addOffsetToExpression(value, offset, length)', function () {
