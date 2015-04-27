87e827f0e1b9a15e0396f9f765e53fdac85e8240
adamrenklint <adam@renklint.com>
2015-04-27 09:45:17 +0200

    need to figure out the performance bottleneck

--- examples/sequences.js
+++ examples/sequences.js
@@ -2,154 +2,103 @@ var bap = require('../index');
 
 function sequences () {
 
-  var sampleKit = bap.kit();
-  var base = bap.sample({
-    src: 'sounds/slices.wav',
-    attack: 0.01,
-    release: 0.01,
+  var lowPianoKit = bap.sample({
+    src: 'sounds/new/own_barricade_end.wav',
+    attack: 0.1,
+    release: 0.1,
+    pitch: -40,
+    pan: 25
+  }).slice(8);
+
+  var piano1 = bap.pattern({ tempo: 84, bars: 2 });
+  piano1.kit('A', lowPianoKit).channel(1).add(
+    ['1.odd.01', 'A3', 96],
+    ['1.even.01', 'A4', 96],
+    ['2.1.01', 'A3', 96],
+    ['2.2.01', 'A4', 96],
+    ['2.3.01', 'A1', 96],
+    ['2.4.01', 'A2', 96]
+  );
+
+  var otherPianoKit = bap.sample({
+    src: 'sounds/new/own_barricade_middle.wav',
+    attack: 0.3,
+    release: 0.1,
+    pitch: -40,
+    pan: -25,
+    volume: 150
+  }).slice(5);
+
+  var otherPianoPattern = bap.pattern({ bars: 2 }).kit('B', otherPianoKit);
+  otherPianoPattern.channel(1).add(
+    ['1.*.52', 'B2', 48],
+    ['1.1.01', 'B1', 52],
+    ['2.*.48', 'B4', 48]
+  );
+
+  var bassKit = bap.kit();
+  bassKit.slot(1).layer(bap.sample({
+    src: 'sounds/new/Sample25.WAV',
+    channel: 'left'
+  }));
+
+  var bassPattern = bap.pattern().kit('A', bassKit);
+  bassPattern.channel(1).add(
+    // ['2.3.01', 'A1']
+  );
+
+  var drumKit = bap.kit();
+  drumKit.slot(1).layer('sounds/kick.wav');
+  drumKit.slot(2).layer('sounds/new/SNARE1.WAV').layer(bap.sample({
+    src: 'sounds/new/SNARE_38.WAV',
+    volume: 40
+  }));
+  drumKit.slot(3).layer(bap.sample({
+    src: 'sounds/hihat.wav',
+    volume: 20
+  }));
+
+  var drumPattern = bap.pattern().kit('A', drumKit);
+  drumPattern.channel(1).add(
+    ['*.1.01', 'A1'],
+    ['*.3.52', 'A1'],
+    ['*.odd.92', 'A2'],
+    ['*.*.%52', 'A3']
+  );
+
+  var breakSample = bap.sample({
+    src: 'sounds/esther.wav',
+    pitch: -48,
+    volume: 30,
     channel: 'right'
   });
-  sampleKit.slot(1).layer(base.with({
-    offset: 0.072,
-    length: 0.719
-  }));
-  sampleKit.slot(2).layer(base.with({
-    offset: 0.9,
-    length: 0.750
-  }));
-  sampleKit.slot(3).layer(base.with({
-    offset: 1.68,
-    length: 0.690
-  }));
-  sampleKit.slot(4).layer(base.with({
-    offset: 9.49,
-    length: 2,
-    reverse: true
-  }));
+  var breakKit = breakSample.slice(16);
+  var smallBreakKit = breakSample.slice(32);
 
-  var breakKit = bap.sample({
-    src: 'sounds/esther.wav',
-    pitch: -26
-  }).slice(16);
-  breakKit.slot(1).layer('sounds/kick.wav');
-  breakKit.slot(2).layer('sounds/snare.wav');
-  breakKit.slot(4).layer('sounds/snare.wav');
-
-  var introSamplePattern = bap.pattern({ bars: 2, tempo: 200 });
-  introSamplePattern.channel(1).add(
-    ['1.1.01', 'A1', 96],
-    ['1.2.01', 'A1', 96],
-    ['1.3.01', 'A1', 96],
-    ['1.4.01', 'A1', 96],
-    ['1.4.01', 'A2', 96],
-    ['2.1.01', 'A2', 96],
-    ['2.2.01', 'A3', 96],
+  var breakPattern = bap.pattern({ tempo: 84, bars: 2 })
+    .kit('A', breakKit)
+    .kit('B', smallBreakKit);
+  breakPattern.channel(1).add(
+    ['*.1.01', 'A1', 96],
+    ['*.1.93', 'A2', 100],
+    ['1.3.01', 'B8', 48],
+    ['1.3.52', 'A1', 48],
+    ['1.3.94', 'A2', 100],
     ['2.3.01', 'A3', 96],
-    ['2.4.01', 'A3', 96]
+    ['2.4.01', 'A4', 96]
   );
 
-  var mainSamplePattern = bap.pattern({ bars: 2, tempo: 95 });
-  mainSamplePattern.channel(1).add(
-    ['1.1.01', 'A1', 96],
-    ['1.2.01', 'A1', 96],
-    ['1.3.01', 'A2'],
-    ['2.1.01', 'A3'],
-    ['2.2.80', 'A4', (96 * 2) + 16 ]
-  );
-
-  var drumPattern = bap.pattern({ bars: 2, tempo: 95 });
-  drumPattern.channel(2).add(
-    ['1.1.01', 'B1'],
-    ['*.even.01', 'B2'],
-    ['1.3.01', 'B3'],
-    // ['1.4.01', 'B4'],
-    ['2.1.01', 'B1'],
-    // ['2.2.01', 'B2'],
-    ['2.3.01', 'B8'],
-    // ['2.4.01', 'B9'],
-    ['2.4.49', 'B5', 48]
-  );
-
-  mainSamplePattern.kit('A', sampleKit);
-  introSamplePattern.kit('A', sampleKit);
-  drumPattern.kit('B', breakKit);
-
-  var sequence = bap.sequence(
-    // [mainSamplePattern, drumPattern],
-    // [mainSamplePattern, drumPattern],
-    [mainSamplePattern, drumPattern],
+  var x = bap.sequence(
+    piano1,
+    [piano1, otherPianoPattern],
+    [piano1, otherPianoPattern, drumPattern, breakPattern],
+    [piano1, otherPianoPattern, drumPattern, breakPattern],
+    [piano1, otherPianoPattern, breakPattern],
+    [piano1, otherPianoPattern, drumPattern, breakPattern],
     { loop: true }
-  );
-
-  // var sequence2 = bap.sequence([
-  //   introSamplePattern,
-  //   sequence,
-  //   sequence,
-  //   sequence
-  // ]);
-
-
-  // var on = false;
-  // function next() {
-  //   if (on) {
-  //     drumPattern.start();
-  //   }
-  //   else {
-  //     sequence.start();
-  //   }
-  //   on = !on;
-  //   setTimeout(next, 3000);
-  // }
-  // next();
-  //
-
-  // var p = bap.pattern()
-  // p.channel(1).add(
-  //   ['*.*.*', 'C1']
-  // );
-  // p.start();
-
-
-
-
-  // setTimeout(function () {
-  //   // s.sequences.push(drumPattern);
-  //   console.log('foo');
-  //   s.sequences = s.sequences.concat(drumPattern);
-  // // //   drumPattern.channel(2).notes.models[3].position = '1.3.49';
-  // }, 3000);
-
-
-
-
-
-
-
-
-
-
-
-  var s = bap.sequence(
-    introSamplePattern,
-    sequence,
-    {loop:true});
-  s.start();
-
-  
-
-
-  // var pattern1 = new bap.pattern();
-  // var pattern2 = new bap.pattern();
-  // pattern1.kit('A', sampleKit).kit('B', breakKit);
-  // pattern2.kit('A', sampleKit).kit('B', breakKit);
-  // pattern1.channel(1).add(['1.1.01', 'A1']);
-  // pattern2.channel(1).add(['*.1.05', 'B1']);
-  // var seq1 = new bap.sequence(pattern1, [pattern1, pattern2]);
-  // var seq2 = new bap.sequence(seq1, seq1);
-  // seq2.notes().forEach(function (n) {
-  //   console.log(n.position, n.key);
-  // });
-  // seq2.start();
+  )
+  x.start();
+  // debugger;
 }
 
 module.exports = sequences;
