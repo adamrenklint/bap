93034a7c9324d14ba019b639e943fba56fdf1683
Adam Renklint <adam@renklint.com>
2015-09-24 23:17:38 +0200

    properly bypassing, but performance is shit, even in chrome

--- examples/boombap.js
+++ examples/boombap.js
@@ -74,49 +74,50 @@ function boombap () {
     attack: 0.01,
     release: 0.01
   }));
-  bassKit.connect(bap.overdrive({ dry: 120, wet: 40, gain: 60 }));
+  var overdrive = bap.overdrive({ dry: 120, wet: 40, gain: 60 });
+  bassKit.connect(overdrive);
 
   var boombapPattern = bap.pattern({ bars: 2, tempo: 90 });
-  boombapPattern.channel(1).add(
-    ['1.1.01', '1Q'],
-    ['1.1.51', '1Q', null, 80],
-    ['1.1.91', '1W'],
-    ['1.2.88', '1Q'],
-    ['1.3.75', '1Q'],
-    ['1.3.91', '1W'],
-    ['1.4.72', '1Q', null, 80],
-    ['2.1.91', '1W'],
-    ['2.1.51', '1Q', null, 70],
-    ['2.3.51', '1Q', null, 80],
-    ['2.3.88', '1Q'],
-    ['2.4.03', '1W']
-  );
-
-  boombapPattern.channel(2).add(
-    ['*.odd.01',  '1E', null, 70],
-    ['*.even.01', '1E', null, 80],
-    ['*.4.53',    '1E', null, 60]
-  );
-
-  boombapPattern.channel(3).add(
-    ['1.1.01', '2Q'],
-    ['1.4.90', '2W', null, 40],
-    ['2.1.52', '2W', null, 70]
-  );
-
-  boombapPattern.channel(4).add(
-    ['1.2.05', '3E', null, 60],
-    ['1.2.51', '3E', null, 40],
-    ['1.3.05', '3E', null, 20],
-    ['1.3.51', '3E', null, 5],
-    ['1.3.75', '3Q', null, 60],
-    ['1.4.52', '3Q', null, 20],
-    ['2.2.05', '3E', null, 60],
-    ['2.2.50', '3W', null, 60],
-    ['2.3.25', '3Q', 70, 60],
-    ['2.4.01', '3Q', 85, 30],
-    ['2.4.75', '3Q', 85, 10]
-  );
+  // boombapPattern.channel(1).add(
+  //   ['1.1.01', '1Q'],
+  //   ['1.1.51', '1Q', null, 80],
+  //   ['1.1.91', '1W'],
+  //   ['1.2.88', '1Q'],
+  //   ['1.3.75', '1Q'],
+  //   ['1.3.91', '1W'],
+  //   ['1.4.72', '1Q', null, 80],
+  //   ['2.1.91', '1W'],
+  //   ['2.1.51', '1Q', null, 70],
+  //   ['2.3.51', '1Q', null, 80],
+  //   ['2.3.88', '1Q'],
+  //   ['2.4.03', '1W']
+  // );
+  //
+  // boombapPattern.channel(2).add(
+  //   ['*.odd.01',  '1E', null, 70],
+  //   ['*.even.01', '1E', null, 80],
+  //   ['*.4.53',    '1E', null, 60]
+  // );
+  //
+  // boombapPattern.channel(3).add(
+  //   ['1.1.01', '2Q'],
+  //   ['1.4.90', '2W', null, 40],
+  //   ['2.1.52', '2W', null, 70]
+  // );
+  //
+  // boombapPattern.channel(4).add(
+  //   ['1.2.05', '3E', null, 60],
+  //   ['1.2.51', '3E', null, 40],
+  //   ['1.3.05', '3E', null, 20],
+  //   ['1.3.51', '3E', null, 5],
+  //   ['1.3.75', '3Q', null, 60],
+  //   ['1.4.52', '3Q', null, 20],
+  //   ['2.2.05', '3E', null, 60],
+  //   ['2.2.50', '3W', null, 60],
+  //   ['2.3.25', '3Q', 70, 60],
+  //   ['2.4.01', '3Q', 85, 30],
+  //   ['2.4.75', '3Q', 85, 10]
+  // );
 
   boombapPattern.channel(5).add(
     ['1.1.01', '4Q', 60, 80, -90],
@@ -129,9 +130,9 @@ function boombap () {
   );
 
   bap.clock.sequence = boombapPattern
-    .kit(1, drumKit)
-    .kit(2, plongKit)
-    .kit(3, stringKit)
+    // .kit(1, drumKit)
+    // .kit(2, plongKit)
+    // .kit(3, stringKit)
     .kit(4, bassKit);
 }
 
